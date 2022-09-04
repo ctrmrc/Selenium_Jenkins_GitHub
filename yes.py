d685a0e1a6b08323a0336078839d328dd164ab4e
ctrmrc <79467547+ctrmrc@users.noreply.github.com>
2022-09-05 01:48:26 +0300

    123

--- yes.py
+++ yes.py
@@ -6,11 +6,17 @@ files = os.listdir(dirname)
 folder_number = re.findall('[0-9]+', str(files))
 max_folder_number = max(folder_number)
 f_read = open(f'C:/data/jenkins_home/jobs/Selenium_Jenkins_GitHub/builds/{max_folder_number}/log', 'r')
-last_line = f_read.readlines()[-1]
-if 'SUCCESS' in last_line:
-    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Finish with SUCCESS"')
+text = f_read.read()
+f_read.close()
+pass_txt = 'PASSED'
+test_start_txt = 'TEST START'
+count_pass_txt = text.count(pass_txt)
+count_test_start_txt = text.count(test_start_txt)
+if count_test_start_txt > count_pass_txt:
+    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Selenium_Jenkins_GitHub Build f'{max_folder_number}' finish with FAILURE"')
+elif count_test_start_txt == count_pass_txt:
+    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Selenium_Jenkins_GitHub Build f'{max_folder_number}' finish with SUCCESS"')
 else:
-    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Finish with FAILURE"')
-
-print(max_folder_number)
-print(f'last_line IS ----!!!!       {last_line}    !!!!!-------')
+    print('ERROR')
+    
+    
