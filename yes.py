e51cdce0c980353a06214c16eafad1d5072a91ed
ctrmrc <79467547+ctrmrc@users.noreply.github.com>
2022-09-05 01:51:13 +0300

    111111111

--- yes.py
+++ yes.py
@@ -13,9 +13,9 @@ test_start_txt = 'TEST START'
 count_pass_txt = text.count(pass_txt)
 count_test_start_txt = text.count(test_start_txt)
 if count_test_start_txt > count_pass_txt:
-    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Selenium_Jenkins_GitHub Build f'{max_folder_number}' finish with FAILURE"')
+    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Selenium_Jenkins_GitHub Build %BUILD_NUMBER% finish with FAILURE"')
 elif count_test_start_txt == count_pass_txt:
-    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Selenium_Jenkins_GitHub Build f'{max_folder_number}' finish with SUCCESS"')
+    os.system('curl -s -X POST https://api.telegram.org/bot5698947661:AAFmFW1PEUa7STMd6yXq9x91tJ2oyavqKAE/sendMessage -d chat_id=-746480341 -d text="Selenium_Jenkins_GitHub Build %BUILD_NUMBER% finish with SUCCESS"')
 else:
     print('ERROR')
     
